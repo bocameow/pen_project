6daefabfae5c8959ed91061880b3d32c020fb713
bocameow <w25909599@mail.fcu.edu.tw>
2021-07-29 12:16:19 +0800

    first commit

---
+++ setMacro.sh
@@ -0,0 +1,2 @@
+alias server="g++ -o server.out server_end.cpp pen.pb.cc -lprotobuf -lpthread; ./server.out; rm server.out"
+alias client="g++ -o client.out client_end.cpp pen.pb.cc -lprotobuf -lpthread; ./client.out; rm client.out"
