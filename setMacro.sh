2a49e61dd16dc4dcd02f17302a939a57061ae79c
bocameow <w25909599@mail.fcu.edu.tw>
2021-07-29 12:36:52 +0800

    add README

--- setMacro.sh
+++ setMacro.sh
@@ -1,2 +1,4 @@
 alias server="g++ -o server.out server_end.cpp pen.pb.cc -lprotobuf -lpthread; ./server.out; rm server.out"
 alias client="g++ -o client.out client_end.cpp pen.pb.cc -lprotobuf -lpthread; ./client.out; rm client.out"
+alias add="git add ."
+alias push="git push -u origin master"
